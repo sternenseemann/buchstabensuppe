913fde909a6c6f935c97cc25a406349e55c26dd0
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-12-08 20:12:09 +0100

    test(bs_decode_utf8): add risky decoding failure test

--- test.c
+++ test.c
@@ -26,4 +26,21 @@ int main(void) {
 
   test_case("Freed buffer has no capacity", family.bs_utf32_buffer_cap == 0 &&
     family.bs_utf32_buffer_len == 0);
+
+  FILE *random = fopen("/dev/urandom", "rb");
+
+  if(random != NULL) {
+    char buf[1024];
+
+    size_t read = fread(buf, 1, sizeof(buf), random);
+
+    if(!ferror(random) && read == sizeof(buf)) {
+      errno = 0;
+      bs_utf32_buffer_t unlikely = bs_decode_utf8(buf, sizeof(buf));
+
+      test_case("Utf8-decoding random stuff failed", errno == EINVAL);
+
+      bs_utf32_buffer_free(&unlikely);
+    }
+  }
 }
